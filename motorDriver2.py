2f4aef7e17bf7d1233a4fc0776e094dcb4f2fed0
Erik5CA <erik.castillo.anzures@gmail.com>
2022-06-23 13:23:35 -0500

    motor driver 2

--- motorDriver2.py
+++ motorDriver2.py
@@ -1,37 +1,36 @@
-#Motor driver 2
+
 import time                     #Para las pausas
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)        #Usar la numeracion de pines de la placa
+GPIO.setmode(GPIO.BCM)        #Usar la numeracion de GPIO
 
-pinDir = 11                    #Pin DIR
-pinStep = 7                    #Pin Step
-numSteps = 200                  #Numero de pasos del motor
-microPausa = 0.005              #Numero de segundos de pausa
+pinDir = 27                    #Pin DIR
+pinStep = 22                    #Pin Step
+# numSteps = 200                  #Numero de pasos del motor
+# microPausa = 0.005              #Numero de segundos de pausa
 
 
 GPIO.setup(pinDir,GPIO.OUT)
 GPIO.setup(pinStep,GPIO.OUT)
-try: 
-    while True:
 
-            GPIO.output(pinDir,0)           #Establezco una direccion (0 o 1)
 
-            for x in range(0,numSteps):
-                    GPIO.output(pinStep, True)
-                    time.sleep(microPausa)
-                    GPIO.output(pinStep, False)
-                    time.sleep(microPausa)
+def adelante(tiempo,numSteps):
+    GPIO.output(pinDir,0)
+    for x in range(0,numSteps):
+        GPIO.output(pinStep, True)
+        time.sleep(tiempo)
+        GPIO.output(pinStep, False)
+        time.sleep(tiempo)
 
-            time.sleep(microPausa)
+def atras(tiempo,numSteps):
+    GPIO.output(pinDir, 1)         
+    for x in range(0,numSteps):
+        GPIO.output(pinStep, True)
+        time.sleep(tiempo)
+        GPIO.output(pinStep, False)
+        time.sleep(tiempo)
 
-            GPIO.output(pinDir, 1)          #Cambio de direccion
-
-            for x in range(0,numSteps):
-                    GPIO.output(pinStep, True)
-                    time.sleep(microPausa)
-                    GPIO.output(pinStep, False)
-                    time.sleep(microPausa)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+def reset():
+    GPIO.output(pinDir,0)
+    GPIO.output(pinStep,0)
