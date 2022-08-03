873427da58346f8373ef90663db049f564c3553f
Erik5CA <103864615+Erik5CA@users.noreply.github.com>
2022-08-02 21:36:41 -0500

    Actualizar posicion

--- carrusel.py
+++ carrusel.py
@@ -1,7 +1,7 @@
 
 #Control del carrusel
-import motorDriver2 as M2
-
+#import motorDriver2 as M2
+#impor motorDriver1 
 posiciones = open("posicion.txt", "rt")
 pos_ini = int(posiciones.readline())
 pos_fin = int(posiciones.readline())
@@ -16,11 +16,13 @@ def default(x,y):
 
 dif = default(pos_ini,pos_fin)
 
-if dif != 0:
-    pos_fin = 1
-    M2.adelante(tiempo,dif*numSteps)
-    posiciones = open("posicion.txt", "wt")
-    posiciones.write(str(pos_ini)+"\n"+str(pos_fin))
-    posiciones.close()
-else:
-    print("Ya estas en la posición 1")
+def actulizarpos():
+    if dif != 0:
+        pos_fin = 1
+        motorDriver1.movMotor(tiempo,dif*numSteps,False,apu)
+        #M2.adelante(tiempo,dif*numSteps)
+        posiciones = open("posicion.txt", "wt")
+        posiciones.write(str(pos_ini)+"\n"+str(pos_fin))
+        posiciones.close()
+    else:
+        print("Ya estas en la posición 1")
