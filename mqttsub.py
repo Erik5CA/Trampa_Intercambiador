a8f8fa154c6b2f4a4a45a37738b167026a155610
Erik5CA <erik.castillo.anzures@gmail.com>
2022-10-11 13:43:52 -0500

    Agregue todos los mqtts para los sensores en pimatic

--- mqttsub.py
+++ mqttsub.py
@@ -1,30 +1,34 @@
-
 import paho.mqtt.client as mqtt
+import carrusel as car
 import RPi.GPIO as GPIO
-import time
-import mqttBMP as BMP
-import mqttDHT as DHT
-import carrusel
-import mqttMotor as Motor
+import mqttSSR as SSR                   # Este archivo ejecuta las acciones en el SSR
+import mqttSensores as mqtts            # Este archivo contiene la configuracion de todos los sensores para mqtt
+import mqttMotor as MOTOR
+import motorDriver2 as M2
+
+# 132.248.51.251
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print(f"Connected with result code {rc}")
     client.subscribe("trampa/#") 
 
 try:
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.on_message = Motor.on_message
+    client.on_message = MOTOR.on_message_MOTOR
 
-    client.connect("test.mosquitto.org", 1883, 60)
-    client.message_callback_add("trampa/sensor/dato/bmp",BMP.on_message_sensores_BMP)
-    client.message_callback_add("trampa/sensor/dato/dht",DHT.on_message_sensores_DHT)
+    client.connect("132.248.51.251", 1883, 60)
+    client.message_callback_add("trampa/sensor/dato/bmp",mqtts.on_message_sensores_BMP)
+    client.message_callback_add("trampa/sensor/dato/dht",mqtts.on_message_sensores_DHT)
+    client.message_callback_add("trampa/sensor/dato/49e",mqtts.on_message_sensores_Hall)
+    client.message_callback_add("trampa/sensor/dato/lm",mqtts.on_message_sensores_LM)
+    client.message_callback_add("trampa/sensor/dato/sct", mqtts.on_message_sensores_SCT)
+    client.message_callback_add("trampa/boton/ssr", SSR.on_message_SSR)
+    client.message_callback_add("trampa/boton/posicion", MOTOR.on_message_MOTOR)
     client.loop_forever()
 
 except KeyboardInterrupt:
-    posiciones = open("posicion.txt", "wt")
-    posiciones.write(str(carrusel.pos_ini)+"\n"+str(carrusel.pos_fin)+"\n"+str(carrusel.dir))
-    posiciones.close()
-    Motor.motorDriver1.reset()
+    with open("posicion.txt", "wt") as f:
+        f.writelines(f"{car.pos_ini}\n{car.pos_fin}")
+    M2.reset()
     GPIO.cleanup()
-
