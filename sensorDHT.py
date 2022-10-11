1bdf6189236ad5c7bf1f8efe4e041cd8fad8c8aa
Erik5CA <erik.castillo.anzures@gmail.com>
2022-10-11 13:34:24 -0500

    Limpie el codigo

--- sensorDHT.py
+++ sensorDHT.py
@@ -1,11 +1,8 @@
-#Sensor DHT
+# Se alimenta con 5V
+
 import Adafruit_DHT as DHT
-import time
 
 sensorDht = DHT.DHT11
-pinDht = 17
+pinDht = 23
 
 humDht, tempDht = DHT.read_retry(sensorDht, pinDht)
-
-# print('Temperatura = {0:0.2f} *C'.format(tempDht))
-# print('Humedad = {0:0.2f} %'.format(humDht))
