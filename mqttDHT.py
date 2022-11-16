024fff56dcb8fb556b40c89d88c178888fe906cb
Erik5CA <erik.castillo.anzures@gmail.com>
2022-11-16 13:23:28 -0600

    codigo final

--- mqttDHT.py
+++ mqttDHT.py
@@ -3,4 +3,3 @@ import sensorDHT as DHT
 def on_message_sensores_DHT(client, userdata, msg):
     client.publish("trampa/sensor/dht11/temperatura", DHT.tempDht)
     client.publish("trampa/sensor/dht11/humedad", DHT.humDht)
-    print("Actualizado")
