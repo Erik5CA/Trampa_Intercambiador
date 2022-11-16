4f50c1647978ebba0ebc9b656d8a1308aa7050db
Erik5CA <erik.castillo.anzures@gmail.com>
2022-11-16 13:30:38 -0600

    Agregé funcion para obtener los datos

--- sensorDHT.py
+++ sensorDHT.py
@@ -2,7 +2,15 @@
 
 import Adafruit_DHT as DHT
 
+
 sensorDht = DHT.DHT11
 pinDht = 23
 
-humDht, tempDht = DHT.read_retry(sensorDht, pinDht)
+def datos():
+    retries = 1
+    try:
+        humDht, tempDht = DHT.read_retry(sensorDht, pinDht, retries)
+        if humDht is not None and tempDht is not None:
+            return humDht,tempDht
+    except TypeError:
+        print("\n\t\tSENSOR DHT11 DESCONECTADO")
